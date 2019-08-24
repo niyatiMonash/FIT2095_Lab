ebbef25bfa3e879ef49abda2ef9538e5aae020a4
niyatisrinivasan <nsri0001@student.monash.edu>
2019-08-24 17:17:02 +1000

    commit 3: finished all the tasks

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 let express = require('express');
 let router = express.Router();
+let bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+router.use(bodyParser.json())
+
 var db = [];
 
 router.get('/', function (req, res) {
@@ -10,12 +19,31 @@ router.get('/', function (req, res) {
     });
 });
 
-router.get('/add-task', function(req,res){
+router.get('/add-task', function (req, res) {
     res.render('add-task.html');
 });
 
-router.get('/list-task', function(req,res){
-    res.render('view-task.html', {username: "Guest", taskDb: db});
+router.post('/addtask', function (req, res) {
+    let name = req.body.taskName
+    let dueDate = req.body.taskDueDate;
+    let description = req.body.taskDescription;
+
+    let task = {
+        taskname: name,
+        taskduedate: dueDate,
+        taskdescription: description
+    }
+    console.log(task);
+    
+    db.push(task);
+    res.send("Thank you for adding a task");
+});
+
+router.get('/list-task', function (req, res) {
+    res.render('view-task.html', {
+        username: "Guest",
+        taskDb: db
+    });
 });
 
 //export this router 
